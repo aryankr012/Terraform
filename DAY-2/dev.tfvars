f8d018e0f97336b5089e8c34f864c8ae2244f203
Aryan <aryankumarktr88@gmail.com>
2026-07-25 13:16:49 +0530

    DAY-2

---
+++ DAY-2/dev.tfvars
@@ -0,0 +1,4 @@
+ami = "ami-0e38835daf6b8a2b9"
+type = "t2.nano"
+key = "AWS_KeyPair"
+
---
+++ DAY-2/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  type    = string
+  default = ""
+}
+
+variable "key" {
+  type    = string
+  default = ""
+}
+
+variable "type" {
+  type    = string
+  default = ""
+
+}
